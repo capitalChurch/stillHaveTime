cee1a805b96b01f15c486b04ca0aefd6948530e9
Kevyn Pinheiro Klava <kevynsax@gmail.com>
2019-04-27 09:34:13 -0300

    Creates Fonts

--- src/components/landingPage/index.js
+++ src/components/landingPage/index.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
+import { ReactComponent as Logo } from "../../assets/images/logo.svg";
+
+import "./landingPage.scss";
 
 export default class LandingPage extends Component{
     render = () => {
-        return (<div>Olá</div>)
+        return (
+            <div className="resultado">
+                <div className="topBar">
+                    <Logo className="logo" />
+                    <div className="square yellow" />
+                </div>
+            </div>
+        )
     }
 }
