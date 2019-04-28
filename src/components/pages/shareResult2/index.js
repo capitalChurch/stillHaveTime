3d27ffb5bc7b2969732067915ee9f8a1882195e6
Kevyn Pinheiro Klava <kevynsax@gmail.com>
2019-04-27 21:34:24 -0300

    Finishing shareResult2

--- src/components/pages/shareResult2/index.js
+++ src/components/pages/shareResult2/index.js
@@ -8,12 +8,14 @@ export default function(props){
             <div className="body">
                 <div className="text">
                     <span className="title">João <span className="accent">e</span><br />Anna</span>
-                    <span className="bodyText">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</span>
+                    <span className="bodyText">It is a long established fact that a reader will be distracted by of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</span>
                 </div>
-                <div className="score">
-                    <span>6Anos</span>
-                    <span>3 meses</span>
-                    <span>8 dias</span>
+                <div className="wrapperScore">
+                    <div className="score">
+                        <span>6Anos</span>
+                        <span>3 meses</span>
+                        <span>8 dias</span>
+                    </div>
                 </div>
             </div>
             <div className="buttonBar">
