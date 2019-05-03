966dcd4708b7604afde105bc15326649730ac7fd
Kevyn Pinheiro Klava <kevynsax@gmail.com>
2019-05-03 12:05:31 -0300

    Correct: button hover; color backButton on Thinking In Some One

--- src/components/pages/thinkingInSomeOne/index.js
+++ src/components/pages/thinkingInSomeOne/index.js
@@ -7,7 +7,7 @@ export default class ThinkingInSomeOne extends Component{
     render = () => (
         <Fragment>
             <div className="gato" />
-            <CleanLayout className="thinkingInSomeOne">
+            <CleanLayout className="thinkingInSomeOne" color="yellow">
                 <span>Agora pense em alguém que você ame e que precise conhecer do amor de Jesus</span>
             </CleanLayout>
         </Fragment>
