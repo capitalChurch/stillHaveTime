112e07f29dcab29741dcfe028e9409fbe9dc29e7
Kevyn Pinheiro Klava <kevynsax@gmail.com>
2019-05-05 13:36:26 -0300

    Coorect bug when goesback from familiarRelation to relation and click in family

--- src/components/pages/relation/index.js
+++ src/components/pages/relation/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import PictureLayout from "../../layouts/pictureLayout";
 import Background from "../../../assets/images/background/ferias_color_bg.jpg";
 import ChooseOne from "../../utils/chooseOne";
-
-import "./relation.scss";
 import {getMyRelation, getRelation, saveRelation} from "../../../model/storage";
 import {changeRoute, typesRelations} from "../../../model/constants";
 import {EnumRotas} from "../../../model/types";
 import {formatBeautiful} from "../../../model/utils";
+
+import "./relation.scss";
 
 export default class Relation extends React.Component{
     state = {
@@ -16,9 +16,12 @@ export default class Relation extends React.Component{
     
     componentWillMount = () => this.setState({relation: getRelation() });
 
-    changeRelation = relation => {
+    changeRelation = (callback = () => {}) => relation => {
         const relationType = typesRelations.find(x => x.label === relation).type
-        this.setState({relation: relationType}, () => saveRelation(relationType));
+        this.setState({relation: relationType}, () => {
+            saveRelation(relationType);
+            callback();
+        });
     };
     
     render = () => {
@@ -28,11 +31,11 @@ export default class Relation extends React.Component{
         const labelsTypesRelations = tpRelations.map(x => x.label);
         
         const relationChoosen = typesRelations.find(x => x.type === this.state.relation);
-        const labelChoosen = tpRelations.find(x => x.type === relationChoosen.type || x.type === relationChoosen.typeSuperior).label;
+        const choosen = tpRelations.find(x => x.type === relationChoosen.type || x.type === relationChoosen.typeSuperior);
         
-        const hasChildren = !!typesRelations.find(x => x.typeSuperior === this.state.relation);
+        const hasChildren = !!typesRelations.find(x => x.typeSuperior === choosen.type);
         const nextPage = hasChildren ? EnumRotas.FamiliarRelation : EnumRotas.VacationTogether;
-        const handleSubmit = () => changeRoute(this.props, nextPage);
+        const handleSubmit = this.changeRelation(() => changeRoute(this.props, nextPage));
             
         return (
             <PictureLayout className="relation" colorLayer="yellow" bgImage={Background} nextPage={nextPage}>
@@ -45,7 +48,7 @@ export default class Relation extends React.Component{
                     </div>
                 </div>
                 <div className="secondColumn">
-                    <ChooseOne itens={labelsTypesRelations} choosen={labelChoosen} onChange={this.changeRelation} onClickChoosen={handleSubmit}/>
+                    <ChooseOne itens={labelsTypesRelations} choosen={choosen.label} onChange={this.changeRelation()} onClickChoosen={handleSubmit}/>
                 </div>
             </PictureLayout>
         )
